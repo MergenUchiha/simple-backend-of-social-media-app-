a323a3d2020bbd749d96434b65453b0f75cf672d
Uchiha <mergendanat11@gmail.com>
2024-04-27 23:17:01 +0500

    v 0.1.0

--- backend/app.js
+++ backend/app.js
@@ -4,19 +4,19 @@
 
 import express from "express";
 import mongoose from "mongoose";
-import router from './routes/user.routes.js';
+import router from "./routes/user.routes.js";
 import blogRouter from "./routes/blog.routes.js";
 
 const app = express();
 
 app.use(express.json());
-app.use("/user",router);//user routes
-app.use("/blogs",blogRouter);//blog routes
+app.use("/user", router); //user routes
+app.use("/blogs", blogRouter); //blog routes
 
 //conecting database and listening server
 mongoose
     .connect(
-        "mongodb+srv://messorem:Uchiha_11@cluster0.fmdcvtt.mongodb.net/Blog?retryWrites=true&w=majority&appName=Cluster0"
+        "mongodb+srv://username:{password}@cluster0.fmdcvtt.mongodb.net/Blog?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then(() => {
         app.listen(5000);
